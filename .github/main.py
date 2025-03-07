a8450191fbbbde5c05dfc3b8105778d3f423ee50
Matyas420 <98987@student.vitkovickastredni.cz>
2025-03-07 09:30:44 +0100

    Flask_routing

--- .github/main.py
+++ .github/main.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#----------- Úvod -----------
+
 @app.route("/")
 def Home_page():
     return "Zde bude úvodní stránka pro náš blog"
@@ -16,16 +18,34 @@ def stranka():
 
 @app.route("/product/<id>")
 def product(id:int):
-    return "Toto bude stránka s produkty"
+    return f"Toto bude stránka s produkty {id}"
 
 @app.route("/category/<jmeno>")
 def category(jmeno):
     return f"Toto bude stránka s kategoriemi {jmeno}"
 
 @app.route("/")
-def uzivatel(jmeno):
-    return f"Profil uživatele: {jmeno}"
+def uzivatel(name):
+    return f"Profil uživatele: {name}"
 
-if __name__=="__main__":
+#-------- První cvičení --------
+
+@app.route("/about")
+def about():
+    return "Informace o stránce"
+
+@app.route("/contact")
+def contact():
+    return "Toto bude stránka s kontakty"
+
+@app.route("/article")
+def article():
+    return "Zobrazí obsah stránku"
+
+@app.route("/article/<id>")
+def arctile(id):
+    return f"Zobrazí přesné informace o uživateli {id}"
+
+if __name__ == "__main__":
     app.run(debug=True)
 
