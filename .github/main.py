033e4a8cb7a38c1ae1f783bc1b23dffbd12bba5e
Matyas420 <98987@student.vitkovickastredni.cz>
2025-03-10 13:09:03 +0100

    template1

--- .github/main.py
+++ .github/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -49,3 +49,8 @@ def arctile(id):
 if __name__ == "__main__":
     app.run(debug=True)
 
+#-------- Nevím něco --------
+
+@app.route('/')
+def home_page():
+    return  render_template('index.html')
