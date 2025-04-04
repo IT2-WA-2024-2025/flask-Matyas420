09066434494c3e5abcdcc87fe4c15e07806abbc7
Matyas420 <98987@student.vitkovickastredni.cz>
2025-04-04 09:30:13 +0200

    time_index
    Zjistil jsem že mi nefunguje ten předchozí commint, takže tohle je finální time_index a teď opravím ten předchozí.

--- .github/main.py
+++ .github/main.py
@@ -1,42 +1,39 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, send_from_directory
+from datetime import datetime
 app = Flask(__name__)
+#---- Pro správné fungování stránky ----
+
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
 
 #----------- Úvod -----------
-
-clanky = [
-    {"title": "" }
-]
-
-@app.route("/")
-def Home_page():
-    return  render_template('index.html', clanky=clanky)
-
 @app.route("/kontakt")
 def kontakt():
     return render_template('contact.html')
 
 @app.route("/product")
 def product():
-    return "Toto bude stránka s produkty"
+    return render_template('product.html')
 
 @app.route("/category")
 def category():
-    return "Toto bude stránka s kategoriemi"
+    return render_template('category.html')
 
 #-------- První cvičení --------
 
 @app.route("/about")
 def about():
-    return "Informace o stránce"
+    return render_template('about.html')
 
 @app.route("/contact")
 def contact():
-    return "Toto bude stránka s kontakty"
+    return render_template('contact.html')
 
 #------- Article ----------
+
 @app.route("/article")
-def clanek():
+def article():
     article = [
     {"Název": "Jak začít s Pythonem", "date": "2025-03-31", "author": "Jan Novák"},
     {"Název": "Flask: Webový framework pro začátečníky", "date": "2025-03-30", "author": "Petr Svoboda"},
@@ -53,8 +50,12 @@ def profil():
     vek = 18
     return render_template("index.html", jmeno=jmeno, vek=vek)
 
-#--------- Výpis článků --------
+#---------- Time -------------
 
+@app.route('/')
+def index():
+    now = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+    return render_template('index.html', datum=now)
 
 if __name__ == "__main__":
     app.run(debug=True)
