8653c9d7703c7c2718bcb3fb7b1a16095b4fffc7
Matyas420 <98987@student.vitkovickastredni.cz>
2025-04-07 13:09:22 +0200

    flask get

--- .github/main.py
+++ .github/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from datetime import datetime
 app = Flask(__name__)
 #---- Pro správné fungování stránky ----
@@ -35,7 +35,7 @@ def contact():
 @app.route("/article")
 def article():
     articles = [ 
-        {"title": "Jak začít s Pythonem", "date": "2025-03-31", "author": "Jan Novák"},
+        {"title": "Jak začít s Pythonem", "date": "2025-03-31", "author": "kyle Crane"},
         {"title": "Flask: Webový framework pro začátečníky", "date": "2025-03-30", "author": "Petr Svoboda"},
         {"title": "Datové struktury v Pythonu", "date": "2025-03-29", "author": "Eva Dvořáková"}
     ]
@@ -56,6 +56,21 @@ def profil():
 def index():
     now = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
     return render_template('index.html', datum=now)
+
+#-------- GET ----------------
+
+@app.route('/get', methods=['GET', 'POST'])
+def index():
+    zprava = None
+    if request.method == 'POST' :
+        jmeno = request.form.get('jmeno')
+        if jmeno:
+            zprava = f'Ahoj, {jmeno}!'
+        else:
+            zprava = 'Prosím, vyplňte jméno.'
+    return render_template('index.html', zprava=zprava)
+        
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
